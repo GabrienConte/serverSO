a7ca9ebdd04a6c8bf8340e941e9dd30f98df836d
GabrienConte <gabrielconte0@gmail.com>
2023-06-26 00:35:32 -0300

    Porta para 8080

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         InicializaLugar();
         Log log = new Log();
-        SimpleSocketServer server = new SimpleSocketServer(80);
+        SimpleSocketServer server = new SimpleSocketServer(8080);
         server.startServer();
 
         // Automatically shutdown in 1 minute
